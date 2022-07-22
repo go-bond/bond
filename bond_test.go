d1b975a4c3185d0ba9629d294523d279f7980455
Marcin Gorzynski <marcin@gorzynski.me>
2022-07-22 14:04:34 +0200

    Move tests to main directory so coverage works properly

--- bond_test.go
+++ bond_test.go
@@ -1,32 +1,31 @@
-package tests
+package bond
 
 import (
 	"os"
 	"testing"
 
-	"github.com/go-bond/bond"
 	"github.com/stretchr/testify/require"
 )
 
 const dbName = "demo"
 
-func setupDatabase(serializer ...bond.Serializer) *bond.DB {
-	options := &bond.Options{}
+func setupDatabase(serializer ...Serializer) *DB {
+	options := &Options{}
 	if len(serializer) > 0 && serializer[0] != nil {
 		options.Serializer = serializer[0]
 	}
 
-	db, _ := bond.Open(dbName, options)
+	db, _ := Open(dbName, options)
 	return db
 }
 
-func tearDownDatabase(db *bond.DB) {
+func tearDownDatabase(db *DB) {
 	_ = db.Close()
 	_ = os.RemoveAll(dbName)
 }
 
 func TestBond_Open(t *testing.T) {
-	db, err := bond.Open(dbName, &bond.Options{})
+	db, err := Open(dbName, &Options{})
 	defer func() { _ = os.RemoveAll(dbName) }()
 
 	require.NoError(t, err)
