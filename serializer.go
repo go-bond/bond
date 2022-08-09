1f47e44f7dd62d8714157d4b225a15da4d4cd969
Marcin Górzyński <marcin@gorzynski.me>
2022-08-09 14:01:17 +0200

    Add generic msgpack generated serializer (#34)

--- serializer.go
+++ serializer.go
@@ -1,8 +1,12 @@
 package bond
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"reflect"
 
+	"github.com/tinylib/msgp/msgp"
 	"github.com/vmihailenco/msgpack/v5"
 )
 
@@ -34,13 +38,52 @@ func (s *JsonSerializer) Deserialize(b []byte, i interface{}) error {
 	return json.Unmarshal(b, i)
 }
 
-type MsgPackSerializer struct {
+type MsgpackSerializer struct {
 }
 
-func (m *MsgPackSerializer) Serialize(i interface{}) ([]byte, error) {
+func (m *MsgpackSerializer) Serialize(i interface{}) ([]byte, error) {
 	return msgpack.Marshal(i)
 }
 
-func (m *MsgPackSerializer) Deserialize(b []byte, i interface{}) error {
+func (m *MsgpackSerializer) Deserialize(b []byte, i interface{}) error {
 	return msgpack.Unmarshal(b, i)
+}
+
+type MsgpackGenSerializer struct {
+}
+
+func (m *MsgpackGenSerializer) Serialize(i interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	e, ok := i.(msgp.Encodable)
+	if !ok {
+		if typ := reflect.TypeOf(i); typ.Kind() == reflect.Ptr {
+			i := reflect.ValueOf(i).Elem().Interface()
+			e, ok = i.(msgp.Encodable)
+			if !ok {
+				return nil, fmt.Errorf("interface does not implement msgp.Encodable")
+			}
+		} else {
+			return nil, fmt.Errorf("interface does not implement msgp.Encodable")
+		}
+	}
+
+	err := msgp.Encode(&buf, e)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func (m *MsgpackGenSerializer) Deserialize(b []byte, i interface{}) error {
+	newEntry := makeNewAny(i)
+	root := findRootInterface(reflect.ValueOf(newEntry))
+	d, ok := root.(msgp.Decodable)
+	if !ok {
+		return fmt.Errorf("interface does not implement msgp.Decodable")
+	}
+
+	err := msgp.Decode(bytes.NewBuffer(b), d)
+	reflect.ValueOf(i).Elem().Set(reflect.ValueOf(newEntry).Elem())
+	return err
 }
--- utils.go
+++ utils.go
@@ -11,3 +11,33 @@ func makeNew[T any]() T {
 		return *new(T)
 	}
 }
+
+func makeNewAny(v any) any {
+	return makeValue(reflect.TypeOf(v)).Interface()
+}
+
+func makeValue(elem reflect.Type) reflect.Value {
+	if elem.Kind() == reflect.Ptr {
+		nextElem := elem.Elem()
+		if nextElem.Kind() == reflect.Ptr {
+			ptrElem := nextElem.Elem()
+			newValue := reflect.New(nextElem)
+			newValue.Elem().Set(makeValue(ptrElem))
+			return newValue
+		}
+		return reflect.New(nextElem)
+	} else {
+		return reflect.New(elem)
+	}
+}
+
+func findRootInterface(v reflect.Value) any {
+	if v.Kind() == reflect.Ptr {
+		if v.Elem().Kind() != reflect.Ptr {
+			return v.Interface()
+		}
+		return findRootInterface(v.Elem())
+	} else {
+		return v.Interface()
+	}
+}
