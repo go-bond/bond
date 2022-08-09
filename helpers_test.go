caa6f4d2eea61bbeb3d836216c33b245b1f62a1b
Marcin Gorzynski <marcin@gorzynski.me>
2022-08-09 15:32:52 +0200

    Add extraction of timestamp and seq num from SequenceNumber

--- helpers_test.go
+++ helpers_test.go
@@ -2,8 +2,10 @@ package bond
 
 import (
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -17,6 +19,39 @@ func TestNumberSequence_Next(t *testing.T) {
 	require.NoError(t, err)
 
 	require.NotEqual(t, id, id2)
+}
+
+func TestNumberSequence_Timestamp(t *testing.T) {
+	IDSequence := &NumberSequence{}
+
+	expectedGreater := time.Now().Unix()
+
+	id, err := IDSequence.Next()
+	require.NoError(t, err)
+
+	ts := IDSequence.Timestamp(id)
+
+	expectedLess := time.Now().Unix()
+
+	assert.GreaterOrEqual(t, ts, uint64(expectedGreater))
+	assert.LessOrEqual(t, ts, uint64(expectedLess))
+}
+
+func TestNumberSequence_SequenceNumber(t *testing.T) {
+	IDSequence := &NumberSequence{}
+
+	id, err := IDSequence.Next()
+	require.NoError(t, err)
+
+	id2, err := IDSequence.Next()
+	require.NoError(t, err)
+
+	ts := IDSequence.SequenceNumber(id)
+
+	ts2 := IDSequence.SequenceNumber(id2)
+
+	assert.Greater(t, ts2, ts)
+	assert.Equal(t, ts+1, ts2)
 }
 
 func TestNumberSequence_Interface(t *testing.T) {
@@ -34,6 +69,7 @@ func TestUUIDGenerator_Next(t *testing.T) {
 	require.NoError(t, err)
 
 	require.NotEqual(t, id, id2)
+
 }
 
 func TestUUIDGenerator_Interface(t *testing.T) {
