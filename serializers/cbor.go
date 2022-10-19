2802d484743a6488a7057deeb2c0bf9e9e25b3ae
Marcin Gorzynski <marcin@gorzynski.me>
2022-10-19 09:45:51 +0200

    CBORSerializer add EncMode / DecMode params

--- serializers/cbor.go
+++ serializers/cbor.go
@@ -5,12 +5,20 @@ import (
 )
 
 type CBORSerializer struct {
+	EncMode cbor.EncMode
+	DecMode cbor.DecMode
 }
 
 func (c *CBORSerializer) Serialize(i interface{}) ([]byte, error) {
+	if c.EncMode != nil {
+		return c.EncMode.Marshal(i)
+	}
 	return cbor.Marshal(i)
 }
 
 func (c *CBORSerializer) Deserialize(b []byte, i interface{}) error {
+	if c.DecMode != nil {
+		return c.DecMode.Unmarshal(b, i)
+	}
 	return cbor.Unmarshal(b, i)
 }
