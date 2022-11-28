3b1644a4a5f297773f3761b434e59e8eb50daec5
poonai <rbalajis25@gmail.com>
2022-11-28 08:19:11 +0530

    add fixed lenght string
    Signed-off-by: poonai <rbalajis25@gmail.com>

--- cmd/tools/immense_bench/main.go
+++ cmd/tools/immense_bench/main.go
@@ -352,3 +352,17 @@ func RandStringRunes(n int) string {
 	}
 	return string(b)
 }
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
