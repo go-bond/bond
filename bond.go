f3b2ed0a3506383b2c2f5cb43e3e929b06d9b2ce
balaji <rbalajis2gmail.com>
2023-10-09 10:54:52 +0530

    add compact method

--- bond.go
+++ bond.go
@@ -217,6 +217,17 @@ func (db *_db) Apply(b Batch, opt WriteOptions) error {
 	return b.Commit(opt)
 }
 
+// Compact the entire bond database
+func (db *_db) Compact() error {
+	// It is sufficient to compact till the maxKey
+	// because most of the keys are less than TableID `0xff`.
+	maxKey := KeyEncode(Key{
+		TableID: 0xff,
+		IndexID: 0xff,
+	})
+	return db.pebble.Compact(nil, maxKey, true)
+}
+
 func (db *_db) Close() error {
 	db.notifyOnClose()
 	return db.pebble.Close()
