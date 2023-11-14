1fac01f23a8ff8679b6010eebd7b53b1c42ccee4
balaji <rbalajis2gmail.com>
2023-11-14 16:56:16 +0530

    add info about value based block filters

--- docs/research/1_Pebble_Collectors_And_Filters/sample/sample_test.go
+++ docs/research/1_Pebble_Collectors_And_Filters/sample/sample_test.go
@@ -418,3 +418,141 @@ func TestBlockFilter_SequDist(t *testing.T) {
 
 	os.RemoveAll(".db")
 }
+
+type BlockIndexValueCollector struct {
+	BlockCollector     *bloom.BloomFilter
+	IndexCollector     *bloom.BloomFilter
+	PrevBlockCollector *bloom.BloomFilter
+	TableCollector     *bloom.BloomFilter
+}
+
+func (b *BlockIndexValueCollector) Name() string {
+	return "block_index_value_collector"
+}
+
+func (b *BlockIndexValueCollector) Add(key sstable.InternalKey, value []byte) error {
+	if b.BlockCollector == nil {
+		b.BlockCollector = bloom.NewWithEstimates(1000, 0.001)
+	}
+	if b.TableCollector == nil {
+		b.TableCollector = bloom.NewWithEstimates(1000, 0.001)
+	}
+	b.BlockCollector.Add(value)
+	b.TableCollector.Add(value)
+	return nil
+}
+
+func (b *BlockIndexValueCollector) FinishDataBlock(buf []byte) ([]byte, error) {
+	buffer := bytes.NewBuffer(buf)
+	_, err := b.BlockCollector.WriteTo(buffer)
+	if err != nil {
+		return nil, err
+	}
+	b.PrevBlockCollector = b.BlockCollector
+	b.BlockCollector = bloom.NewWithEstimates(1000, 0.001)
+	return buffer.Bytes(), nil
+}
+
+func (b *BlockIndexValueCollector) AddPrevDataBlockToIndexBlock() {
+	if b.IndexCollector == nil {
+		b.IndexCollector = bloom.NewWithEstimates(1000, 0.001)
+	}
+	if b.PrevBlockCollector != nil {
+		b.IndexCollector.Merge(b.PrevBlockCollector)
+	}
+}
+
+func (b *BlockIndexValueCollector) FinishIndexBlock(buf []byte) ([]byte, error) {
+	buffer := bytes.NewBuffer(buf)
+	_, err := b.IndexCollector.WriteTo(buffer)
+	if err != nil {
+		return nil, err
+	}
+	b.IndexCollector = bloom.NewWithEstimates(1000, 0.001)
+	return buffer.Bytes(), nil
+}
+
+func (b *BlockIndexValueCollector) FinishTable(buf []byte) ([]byte, error) {
+	buffer := bytes.NewBuffer(buf)
+	_, err := b.TableCollector.WriteTo(buffer)
+	if err != nil {
+		return nil, err
+	}
+	b.TableCollector = bloom.NewWithEstimates(1000, 0.001)
+	return buffer.Bytes(), nil
+}
+
+type BlockIndexValueFilter struct {
+	Values []byte
+}
+
+func (b BlockIndexValueFilter) Name() string {
+	return "block_index_value_collector"
+}
+
+func (b BlockIndexValueFilter) Intersects(props []byte) (bool, error) {
+	// if len(props) == 0 {
+	// 	return true, nil
+	// }
+	bf := &bloom.BloomFilter{}
+	_, err := bf.ReadFrom(bytes.NewBuffer(props))
+	if err != nil {
+		return false, err
+	}
+	exist := bf.Test(b.Values)
+	return exist, nil
+}
+
+func TestDeleteSurface(t *testing.T) {
+	opt := bond.DefaultPebbleOptions()
+	opt.Comparer = pebble.DefaultComparer
+	opt.DisableAutomaticCompactions = true
+	opt.BlockPropertyCollectors = []func() pebble.BlockPropertyCollector{func() pebble.BlockPropertyCollector {
+		return &BlockIndexValueCollector{}
+	}}
+	opt.DisableWAL = true
+
+	opt.FormatMajorVersion = pebble.FormatBlockPropertyCollector
+	pdb, err := pebble.Open(".db", opt)
+	require.NoError(t, err)
+	for i := 0; i < 100; i++ {
+		pdb.Set([]byte(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d", i)), pebble.NoSync)
+	}
+	pdb.Flush()
+	// delete 50 entries.
+	for i := 50; i < 100; i++ {
+		pdb.Delete([]byte(fmt.Sprintf("%d", i)), pebble.NoSync)
+	}
+	pdb.Flush()
+	pdb.Close()
+	pdb, err = pebble.Open(".db", opt)
+	require.NoError(t, err)
+	itr, err := pdb.NewIter(&pebble.IterOptions{
+		PointKeyFilters: []pebble.BlockPropertyFilter{
+			&BlockIndexValueFilter{Values: []byte("50")},
+		},
+	})
+	require.NoError(t, err)
+	exist := false
+	for itr.First(); itr.Valid(); itr.Next() {
+		if string(itr.Value()) == "50" {
+			exist = true
+		}
+	}
+	if exist {
+		fmt.Println("50 supposed to be deleted")
+	}
+	itr, err = pdb.NewIter(&pebble.IterOptions{})
+	require.NoError(t, err)
+	exist = false
+	for itr.First(); itr.Valid(); itr.Next() {
+		if string(itr.Value()) == "50" {
+			exist = true
+		}
+	}
+	if !exist {
+		fmt.Println("50 didn't show when filters not applied")
+	}
+	pdb.Close()
+	os.RemoveAll(".db")
+}
