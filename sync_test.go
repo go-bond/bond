8bc325d416508fe56f63900b2123361f9e96c586
José Nieto <jose@xiam.io>
2025-11-14 12:04:38 -0600

    bond: improve testing

--- sync_test.go
+++ sync_test.go
@@ -1,14 +1,43 @@
 package bond
 
 import (
+	"io"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
+// simpleBatchForSync is a minimal mock for Batch interface to avoid circular dependencies
+type simpleBatchForSync struct{}
+
+func (m *simpleBatchForSync) ID() uint64      { return 0 }
+func (m *simpleBatchForSync) Type() BatchType { return BatchTypeReadWrite }
+func (m *simpleBatchForSync) Len() int        { return 0 }
+func (m *simpleBatchForSync) Count() uint32   { return 0 }
+func (m *simpleBatchForSync) Empty() bool     { return true }
+func (m *simpleBatchForSync) Reset()          {}
+func (m *simpleBatchForSync) Get(key []byte, batch ...Batch) (data []byte, closer io.Closer, err error) {
+	return nil, nil, nil
+}
+func (m *simpleBatchForSync) Set(key []byte, value []byte, opt WriteOptions, batch ...Batch) error {
+	return nil
+}
+func (m *simpleBatchForSync) Delete(key []byte, opt WriteOptions, batch ...Batch) error { return nil }
+func (m *simpleBatchForSync) DeleteRange(start []byte, end []byte, opt WriteOptions, batch ...Batch) error {
+	return nil
+}
+func (m *simpleBatchForSync) Iter(opt *IterOptions, batch ...Batch) Iterator { return nil }
+func (m *simpleBatchForSync) Apply(b Batch, opt WriteOptions) error          { return nil }
+func (m *simpleBatchForSync) Commit(opt WriteOptions) error                  { return nil }
+func (m *simpleBatchForSync) Close() error                                   { return nil }
+func (m *simpleBatchForSync) OnCommit(f func(b Batch) error)                 {}
+func (m *simpleBatchForSync) OnCommitted(f func(b Batch))                    {}
+func (m *simpleBatchForSync) OnError(f func(b Batch, err error))             {}
+func (m *simpleBatchForSync) OnClose(f func(b Batch))                        {}
+
 func Test_SyncBatch(t *testing.T) {
-	syncBatch := NewSyncBatch(&MockBatch{})
+	syncBatch := NewSyncBatch(&simpleBatchForSync{})
 
 	syncChan := make(chan bool)
 	go func() {
